e26e3db3e5ca86630972a32e7abc87b505a882ad
Ivan Ilin <ilyin1997@icloud.com>
2023-12-23 13:38:48 +0300

    add method security

--- src/main/java/com/example/hw_hibernate/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/hw_hibernate/configurations/SecurityConfiguration.java
@@ -1,16 +1,16 @@
-package com.example.hw_hibernate.Configurations;
+package com.example.hw_hibernate.configurations;
 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
@@ -22,17 +22,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
-                .withUser("root")
-                .password(encoder().encode("root"))
-                .roles("ROOT");
+                .withUser("read")
+                .password(encoder().encode("read"))
+                .roles("READ")
+                .and()
+                .withUser("write")
+                .password(encoder().encode("write"))
+                .roles("WRITE")
+                .and()
+                .withUser("delete")
+                .password(encoder().encode("delete"))
+                .roles("DELETE");
+
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeHttpRequests().antMatchers(HttpMethod.GET, "/persons").permitAll()
-                .and()
-                .authorizeHttpRequests().anyRequest().authenticated();
-    }
+
 }
